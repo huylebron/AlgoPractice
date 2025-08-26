d5545e59c375db060f15fa7a9a7c0d175153451f
huylebron <doquochuy27102003@gmail.com>
2025-08-26 16:50:27 +0700

    1221. Split a String in Balanced Strings

---
+++ LeetCode/String/SplitaStringinBalancedStrings.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.String;
+
+public class SplitaStringinBalancedStrings
+{
+    public int BalancedStringSplit(string s)
+    {
+        int count = 0;
+        int balance = 0;
+
+        foreach (var chr in s)
+        {
+            if (chr == 'L') balance += 1 ;
+            else
+            {
+                balance -= 1; 
+            }
+
+            if (balance == 0)
+            {
+                count++;
+                
+            }
+            
+            
+            
+            
+        }
+
+        return count; 
+
+    }
+}
