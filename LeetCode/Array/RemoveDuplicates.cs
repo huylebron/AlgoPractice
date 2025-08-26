37b40d9e5ae25556561f8ebd8dbbcdc3ecf97d8c
huylebron <doquochuy27102003@gmail.com>
2025-08-26 11:46:02 +0700

    Add initial project structure and implementations for LeetCode and CodeWars challenges

---
+++ LeetCode/Array/RemoveDuplicates.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Array;
+
+public class RemoveDuplicates
+{
+    public int RemoveDuplicatesImp(int[] nums)
+    {
+        if (nums.Length == 0) return 0;
+
+
+        int i = 0; // chi so gih 
+        for (int j = 1; j < nums.Length; j++)
+        {
+            if (nums[j] != nums[i])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+            
+            
+        }
+
+        return i + 1; 
+
+    }
+
+
+    public static void Main(string[] args)
+    {
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+        RemoveDuplicates sol = new RemoveDuplicates();
+        int newLength = sol.RemoveDuplicatesImp(nums);
+
+        Console.WriteLine($"Độ dài mới:{newLength} ");
+
+        Console.Write("Mảng sau khi xóa trùng: ");
+        for (int i = 0; i < newLength; i++)
+        {
+            Console.Write($"{nums[i]}");
+        }
+    }
+}
---
+++ LeetCode/Array/SortedSquares.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode.Array;
+
+public class SortedSquares
+{
+    public int[] SortedSquaresImp(int[] nums)
+    {
+        int n = nums.Length;
+        int[] res = new int[n];
+
+
+        int left = 0;
+        int right = n - 1;
+        int resIndex = n - 1;
+
+
+        while (left <= right)
+        {
+            int left_val = Math.Abs(nums[left]);
+            int right_val = Math.Abs(nums[right]);
+
+
+            if (left_val > right_val)
+            {
+
+                res[resIndex] = left_val * left_val;
+                left++;
+                
+            }
+            else
+            {
+                res[resIndex] = right_val * right_val;
+                right--;
+            }
+
+            resIndex--;
+
+
+
+        }
+
+        return res;
+    }
+}
