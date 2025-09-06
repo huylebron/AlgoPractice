2de572c253986ee4e08e6f646dbe33511a30b80a
huylebron <doquochuy27102003@gmail.com>
2025-09-06 16:25:15 +0700

    Add Edabit project and implement HowManyDsAreThere challenge

---
+++ Edabit/HowManyDsAreThere.cs
@@ -0,0 +1,17 @@
+﻿namespace Edabit;
+
+public class HowManyDsAreThere
+{
+    public static int CountDs(string s)
+    {
+
+        return s.Count(Ch => char.ToLowerInvariant(Ch) == 'd');
+        
+        
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(CountDs("My friend Dylan got distracted in school."));
+    }
+}
+
